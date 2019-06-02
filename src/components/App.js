f3cf1f958369e77ed40c1a365ed2192c19da90ac
OoyamaYoshito <b1016006@gmail.com>
2019-06-02 11:38:45 +0900

    Add todolist minimal features

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      todos: []
+    }
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+
+  //タスク追加
+  handleAdd(e){
+    e.preventDefault()
+    if(e.target.text.value == "")return;
+    this.state.todos.push({title: e.target.text.value})
+    this.setState({todos: this.state.todos})
+    e.target.text.value = ''
+  }
+
+  // タスク削除
+  handleDelete(i){
+    this.state.todos.splice(i,1)
+    this.setState({todos: this.state.todos})
+  }
+
+  render(){
   return (
     <div className="App">
-
+      <Form handleAdd={this.handleAdd}/>
+      <List todos={this.state.todos} handleRemove={this.handleDelete}/>
     </div>
   );
+  }
 }
+
+const Form = (props) => (
+  <form onSubmit={props.handleAdd}>
+      <label >Task Add</label>
+      <input name="text" placeholder="text" type="text"/><input type="submit" value="Add"/>
+  </form>
+);
+
+const List = (props) => (
+  <ul>
+    {props.todos.map((todos, i) => {
+      return <li key={i} >{todos.title} <button onClick={() => props.handleRemove(i)}>Delete</button>
+      </li>
+    })}
+  </ul>
+)
 
 export default App;
